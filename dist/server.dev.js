bac7347f4820be98d1e0b4c9f25babd4b0f07e8b
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-05-22 11:46:39 -0500

    update-agencies

---
+++ dist/server.dev.js
@@ -0,0 +1,33 @@
+"use strict";
+
+require("dotenv").config();
+
+var express = require("express");
+
+var mongoose = require("./db/connection");
+
+var tripsController = require("./controllers/trip");
+
+var agencyController = require("./controllers/agency");
+
+var app = express();
+var PORT = process.env.PORT; //imports
+
+var cors = require("cors");
+
+var morgan = require("morgan"); //midleware
+
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+app.get("/", function (req, res) {
+  res.json({
+    hello: "Hello World!"
+  });
+});
+app.use('/trips', tripsController);
+app.use('/agencies', agencyController);
+app.listen(PORT, function () {
+  console.log("Your are listening on port ".concat(PORT));
+});
